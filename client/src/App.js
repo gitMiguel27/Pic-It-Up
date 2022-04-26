5812a9a824a0f54e3cbc8625aebc91a9fb87ff1e
Miguel Nazario <mnazario35@gmail.com>
2022-04-26 17:48:08 -0400

    Got submit to console jpeg file

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event)
+    console.log(event.target[0].value)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Upload File:
+        <input type="file" accept="image/png, image/jpg"/>
+        </label>
+        <br/>
+        <button type={"submit"}>Submit</button>
+      </form>
     </div>
   );
 }
